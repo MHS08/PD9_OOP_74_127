c00bf7a668799e587499b6704e202449d2396fe9
Muhammad Hamza Saeed <81794794+MHS08@users.noreply.github.com>
2021-04-24 17:23:14 +0500

    Task1

---
+++ Manual_PD9/src/Task1.java
@@ -0,0 +1,78 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MHS
+ */
+public class Task1 {
+    
+  static int wordCounter(String s)
+  {
+    int wordCount=0;
+    int size=s.length();
+    for(int i=0;i<size;i++)
+    {
+      if(s.charAt(i) == ' ')
+      {
+        wordCount++;
+      }
+    }
+    wordCount=wordCount+1;
+    return wordCount;
+  };
+  //
+  static int vowelCounter(String s)
+  {
+    int vowelCount=0;
+    int size=s.length();
+    for(int i=0;i<size;i++)
+    {
+      if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
+      {
+        vowelCount++;
+      }
+    }
+    
+    return vowelCount;
+  };
+  static int puncCount(String s)
+  {
+    int puncCount=0;
+    int size=s.length();
+    for(int i=0;i<size;i++)
+    {
+      if(s.charAt(i) == '.' || s.charAt(i) == ',' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '!')
+      {
+        puncCount++;
+      }
+    }
+    
+    return puncCount;
+  };
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+    System.out.println("Enter String");
+    String s=input.nextLine();
+    int word = wordCounter(s);
+    int vowel = vowelCounter(s);
+    int punc = puncCount(s);
+    
+    System.out.println("Number of words:"+word);
+    System.out.println("Number of vowels:"+vowel);
+    System.out.println("Number of punctuations:"+punc);
+    
+        // TODO code application logic here
+    }
+    
+}
