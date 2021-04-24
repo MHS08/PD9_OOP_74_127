ae2a921be77d95967181bf09bea430c08ccfaf31
ahmadrao17 <81874102+ahmadrao17@users.noreply.github.com>
2021-04-24 18:10:42 +0500

    Some Changes in Task 1

--- src/Task1.java
+++ src/Task1.java
@@ -6,74 +6,73 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author MHS
  */
 public class Task1 {
-    
-  static int wordCounter(String s)
-  {
-    int wordCount=0;
-    int size=s.length();
-    for(int i=0;i<size;i++)
-    {
-      if(s.charAt(i) == ' ')
-      {
-        wordCount++;
-      }
+
+    static int wordCounter(String s) {
+        int spcCount = 0;
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) == ' ') {
+                spcCount++;
+            }
+        }
+        int wordCount = ++spcCount;
+        return wordCount;
     }
-    wordCount=wordCount+1;
-    return wordCount;
-  };
+
+    ;
   //
-  static int vowelCounter(String s)
-  {
-    int vowelCount=0;
-    int size=s.length();
-    for(int i=0;i<size;i++)
-    {
-      if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
-      {
-        vowelCount++;
-      }
+  static int vowelCounter(String s) {
+        int vowelCount = 0;
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
+                    || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
     }
-    
-    return vowelCount;
-  };
-  static int puncCount(String s)
-  {
-    int puncCount=0;
-    int size=s.length();
-    for(int i=0;i<size;i++)
-    {
-      if(s.charAt(i) == '.' || s.charAt(i) == ',' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '!')
-      {
-        puncCount++;
-      }
+
+    ;
+  static int puncCount(String s) {
+        int puncCount = 0;
+        int size = s.length();
+        for (int i = 0; i < size; i++) {
+            if (s.charAt(i) == '.' || s.charAt(i) == ',' || s.charAt(i) == ':' || s.charAt(i) == ';' || s.charAt(i) == '!'
+                    || s.charAt(i) == '?' || s.charAt(i) == '-' || s.charAt(i) == '_' || s.charAt(i) == '(' || s.charAt(i) == ')'
+                    || s.charAt(i) == '[' || s.charAt(i) == ']' || s.charAt(i) == '\"' || s.charAt(i) == '\'' || s.charAt(i) == '/') {
+                puncCount++;
+            }
+        }
+
+        return puncCount;
     }
-    
-    return puncCount;
-  };
+
+    ;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-    System.out.println("Enter String");
-    String s=input.nextLine();
-    int word = wordCounter(s);
-    int vowel = vowelCounter(s);
-    int punc = puncCount(s);
-    
-    System.out.println("Number of words:"+word);
-    System.out.println("Number of vowels:"+vowel);
-    System.out.println("Number of punctuations:"+punc);
-    System.out.println("Hello World");
-    
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String");
+        String s = input.nextLine();
+        int word = wordCounter(s);
+        int vowel = vowelCounter(s);
+        int punc = puncCount(s);
+
+        System.out.println("Number of words:" + word);
+        System.out.println("Number of vowels:" + vowel);
+        System.out.println("Number of punctuations:" + punc);
+        System.out.println("Hello World");
+
         // TODO code application logic here
     }
-    
+
 }
