978ca8d370e129c52c5440811111c1177295efed
ahmadrao17 <81874102+ahmadrao17@users.noreply.github.com>
2021-04-24 23:41:59 +0500

    Task3 Changes

--- src/Task3.java
+++ src/Task3.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author G3NZ
  */
 public class Task3 {
+
+    static String ridMultipleBlank(String s) {
+        return s.replaceAll("( )+", " ");
+    }
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter String");
         String s = input.nextLine();
-        String output = s.replaceAll("( )+", " ");
+        String output = ridMultipleBlank(s);
         System.out.println(output);
     }
 }
