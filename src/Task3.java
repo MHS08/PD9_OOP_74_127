709383f61d8b45ace829c5060cae50ca6672d832
ahmadrao17 <81874102+ahmadrao17@users.noreply.github.com>
2021-04-24 22:29:50 +0500

    task 3 start

---
+++ src/Task3.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author G3NZ
+ */
+public class Task3 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter String");
+        String s = input.nextLine();
+        String output = s.trim();
+        System.out.println(output);
+    }
+}
